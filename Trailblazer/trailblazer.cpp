31934bcf1d381e13ed7efca501004f3d763fc78f
lqrrain <lqrrain@gmail.com>
2016-02-09 22:17:25 -0500

    DFS, BFS, Dijkstra and A* have been finished

--- Trailblazer/trailblazer.cpp
+++ Trailblazer/trailblazer.cpp
@@ -6,10 +6,14 @@
 #include "costs.h"
 #include "trailblazer.h"
 #include "queue.h"
+#include "pqueue.h"
 // TODO: include any other headers you need; remove this comment
 using namespace std;
 
 bool dfsHelper(BasicGraph& graph, Vertex* start, Vertex* end, Vector<Vertex*>& path);
+void tracePath(Vertex* start, Vertex* end, Vector<Vertex*>& path);
+void reverseVector(Vector<Vertex*>& path);
+Vector<Vertex*> aStarHelper(BasicGraph& graph, Vertex* start, Vertex* end, bool useHeuristic);
 
 Vector<Vertex*> depthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
     // TODO: implement this function; remove these comments
@@ -28,18 +32,44 @@ bool dfsHelper(BasicGraph& graph, Vertex* start, Vertex* end, Vector<Vertex*>& p
     path.add(start);
     start->setColor(GREEN);
 
-    if(start==end) return true;
+    if(start==end){
+        path.add(start);
+        return true;
+    }
 
-    for(Edge* e : start->arcs){
+    for(Edge* e : start->edges){
         if(!e->finish->visited)
             if(dfsHelper(graph, e->finish, end, path)) return true;
+
     }
 
     path.remove(path.size()-1);
-    start->visited = false;
+    //start->visited = false;
     start->setColor(GRAY);
     return false;
 
+}
+
+void tracePath(Vertex* start, Vertex* end, Vector<Vertex*>& path){
+    if(end == start){        
+        path.add(start);
+        return;
+    }
+    path.add(end);
+    tracePath(start, end->previous, path);
+}
+
+void reverseVector(Vector<Vertex*>& path){
+    int i = 0;
+    int j = path.size()-1;
+
+    while(i>=0&&j<path.size()&&i<=j){
+        Vertex* temp = path[i];
+        path[i] = path[j];
+        path[j] = temp;
+        i++;
+        j--;
+    }
 }
 
 Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end) {
@@ -52,24 +82,31 @@ Vector<Vertex*> breadthFirstSearch(BasicGraph& graph, Vertex* start, Vertex* end
     Vector<Vertex*> path;
 
     Queue<Vertex*> queue;
+    //Add first vertex to queue
+    start->setColor(YELLOW);
+    start->visited = true;
     queue.enqueue(start);
+
+    //bfs
     while(!queue.isEmpty()){
         Vertex* curr = queue.dequeue();
         curr->setColor(GREEN);
+
         if(curr==end){
-            while(curr!=start){
-                path.add(curr);
-                curr = curr->previous;
-            }
-            path.add(start);
+            curr->setColor(GREEN);
+            tracePath(start, curr, path);
+            reverseVector(path);
             return path;
         }
+
         for (Edge* e : curr->edges) {
-            if(!e->finish->visited){
-                e->finish->previous = e->start;
-                e->finish->visited=true;
-                queue.enqueue(e->finish);
-            }
+            if(e->finish->visited) continue;
+
+            e->finish->previous = curr;
+            e->finish->visited = true;
+            queue.enqueue(e->finish);
+            e->finish->setColor(YELLOW);
+
         }
     }
 
@@ -81,17 +118,56 @@ Vector<Vertex*> dijkstrasAlgorithm(BasicGraph& graph, Vertex* start, Vertex* end
     //       (The function body code provided below is just a stub that returns
     //        an empty vector so that the overall project will compile.
     //        You should remove that code and replace it with your implementation.)
+    return aStarHelper(graph, start, end, false);
+}
+
+
+Vector<Vertex*> aStarHelper(BasicGraph& graph, Vertex* start, Vertex* end, bool useHeuristic){
+    graph.resetData();
+
     Vector<Vertex*> path;
+    PriorityQueue<Vertex*> pqueue;
+    for(Vertex* v:graph.getVertexSet()){
+        v->cost = POSITIVE_INFINITY;
+    }
+
+    start->cost = 0.0;
+    double priority = useHeuristic ? heuristicFunction(start, end) : 0.0;
+    pqueue.enqueue(start, priority);
+
+    while(!pqueue.isEmpty()){
+        Vertex* curr = pqueue.dequeue();
+        curr->setColor(GREEN);
+        curr->visited = true;
+
+        if(curr==end){
+            curr->setColor(GREEN);
+            tracePath(start, curr, path);
+            reverseVector(path);
+            return path;
+        }
+
+        for(Edge* e : curr->edges){
+            Vertex* next = e->finish;
+            double cost = curr->cost + e->cost;
+            if(cost < next->cost){
+                double priority = useHeuristic ? cost + heuristicFunction(next, end) : cost;
+                next->cost = cost;
+                next->previous = curr;
+                if(next->getColor() == YELLOW){
+                    pqueue.changePriority(next, priority);
+                }else{
+                    pqueue.enqueue(next, priority);
+                }
+                next->setColor(YELLOW);
+            }
+        }
+    }
     return path;
 }
 
 Vector<Vertex*> aStar(BasicGraph& graph, Vertex* start, Vertex* end) {
-    // TODO: implement this function; remove these comments
-    //       (The function body code provided below is just a stub that returns
-    //        an empty vector so that the overall project will compile.
-    //        You should remove that code and replace it with your implementation.)
-    Vector<Vertex*> path;
-    return path;
+    return aStarHelper(graph, start, end, true);
 }
 
 Set<Edge*> kruskal(BasicGraph& graph) {
